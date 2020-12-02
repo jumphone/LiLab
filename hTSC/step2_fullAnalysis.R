cf9518b17267fc19798641e21d05b885ef974ded
Zhang, Feng <15110700005@fudan.edu.cn>
2020-12-02 16:04:02 +0800

    Update step2_fullAnalysis.R

--- hTSC/step2_fullAnalysis.R
+++ hTSC/step2_fullAnalysis.R
@@ -14,7 +14,8 @@ MAT[1:5,1:5]
 
 
 GRP=as.character(read.table('https://raw.githubusercontent.com/jumphone/BEER/master/SUP/KEGG_Ribosome.txt',sep='\t')[,1])
-GCC=as.character(read.table('https://raw.githubusercontent.com/jumphone/BEER/master/SUP/KEGG_CellCycle.txt',sep='\t')[,1])
+GCC1=as.character(read.table('https://raw.githubusercontent.com/jumphone/BEER/master/SUP/G1S.txt',sep='\t')[,1])
+GCC2=as.character(read.table('https://raw.githubusercontent.com/jumphone/BEER/master/SUP/G2M.txt',sep='\t')[,1])
 
 
 pbmc <- CreateSeuratObject(counts = MAT,  project = "hTSC", min.cells = 0, min.features = 0)
@@ -22,25 +23,34 @@ pbmc <- CreateSeuratObject(counts = MAT,  project = "hTSC", min.cells = 0, min.f
 COUNT=as.matrix(pbmc@assays$RNA@counts)
 SUM=apply(COUNT, 2,sum)
 SRP=apply(COUNT[which(rownames(COUNT) %in% GRP),],2,sum)/ SUM
-SCC=apply(COUNT[which(rownames(COUNT) %in% GCC),],2,sum)/ SUM
-pbmc[['cc']]=SCC
+SCC1=apply(COUNT[which(rownames(COUNT) %in% GCC1),],2,sum)/ SUM
+SCC2=apply(COUNT[which(rownames(COUNT) %in% GCC2),],2,sum)/ SUM
+pbmc[['cc1']]=SCC1
+pbmc[['cc2']]=SCC2
 pbmc[['rp']]=SRP
 ######################
 
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 all.genes <- rownames(pbmc)
-pbmc <- ScaleData(pbmc, features = all.genes, vars.to.regress=c('cc','rp'))
-
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
 #VariableFeaturePlot(pbmc)
+pbmc <- ScaleData(pbmc, features = VariableFeatures(object = pbmc), vars.to.regress=c('cc1','cc2','rp'))
+
+#pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
+#VariableFeaturePlot(pbmc)
+#pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc), npcs = 50)
 pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc), npcs = 50)
 
-DimPlot(pbmc, reduction = "pca")
+#ElbowPlot(pbmc, ndims=50)
+#pbmc <- RunUMAP(pbmc, dims = 1:10,min.dist=0.3,n.neighbors=15)
+#DimPlot(pbmc, reduction = "umap", pt.size=3)
+
+DimPlot(pbmc, reduction = "pca",dims=c(1,2), pt.size=3)
 saveRDS(pbmc, file='pbmc.rds')
 ###############################################################################
 
 
-DimPlot(pbmc, reduction = "pca")+ NoLegend()
+DimPlot(pbmc, reduction = "pca", pt.size=3) #+ NoLegend()
 
 
 
