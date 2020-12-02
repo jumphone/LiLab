96d5eedb7482320afc35e3a8a6e7abaaad63d387
Zhang, Feng <15110700005@fudan.edu.cn>
2020-12-02 15:26:04 +0800

    Update step1_fullAnalysis.R

--- hTSC/step1_fullAnalysis.R
+++ hTSC/step1_fullAnalysis.R
@@ -7,23 +7,51 @@ MAT=read.table('MAT_FULL.NAME.txt',header=T,row.names=1,sep='\t')
 MAT[1:5,1:5]
 
 
+
+GRP=as.character(read.table('https://raw.githubusercontent.com/jumphone/BEER/master/SUP/KEGG_Ribosome.txt',sep='\t')[,1])
+GCC=as.character(read.table('https://raw.githubusercontent.com/jumphone/BEER/master/SUP/KEGG_CellCycle.txt',sep='\t')[,1])
+
+
+
+
 library(reticulate)
 use_python("/home/toolkit/local/bin/python3",required=T)
 py_config()
 
 
 
-pbmc <- CreateSeuratObject(counts = MAT,  project = "HairSkin", min.cells = 0, min.features = 0)
+pbmc <- CreateSeuratObject(counts = MAT,  project = "hTSC", min.cells = 0, min.features = 0)
+###################
+COUNT=as.matrix(pbmc@assays$RNA@counts)
+SUM=apply(COUNT, 2,sum)
+SRP=apply(COUNT[which(rownames(COUNT) %in% GRP),],2,sum)/ SUM
+SCC=apply(COUNT[which(rownames(COUNT) %in% GCC),],2,sum)/ SUM
+pbmc[['cc']]=SCC
+pbmc[['rp']]=SRP
+######################
 
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
-pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+#VariableFeaturePlot(pbmc)
 all.genes <- rownames(pbmc)
-pbmc <- ScaleData(pbmc, features = all.genes)
+#pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc <- ScaleData(pbmc, features = all.genes, vars.to.regress=c('cc','rp'))
 #pbmc <- ScaleData(pbmc, features = VariableFeatures(pbmc))
-pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc), npcs = 30)
-#pbmc <- RunUMAP(pbmc, dims = 1:10,min.dist=0.3,n.neighbors=30)
-
-
+#pbmc <- RunPCA(pbmc, features = all.genes, npcs = 50)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc), npcs = 50)
+#ElbowPlot(pbmc, ndims=50)
+#pbmc <- RunUMAP(pbmc, dims = 1:30,min.dist=0.3,n.neighbors=10)
+#DimPlot(pbmc, reduction = "umap")
 
 DimPlot(pbmc, reduction = "pca")
+saveRDS(pbmc, file='pbmc.rds')
+###############################################################################
+
+
+DimPlot(pbmc, reduction = "pca")+ NoLegend()
+
+
+
+
+
 
