34b065a806f684d19ea629f242803c9cc1386790
Zhang, Feng <15110700005@fudan.edu.cn>
2020-12-18 17:33:37 +0800

    Update step5_newCut_fold_rpkm1_etc.R

--- hTSC/step5_newCut_fold_rpkm1_etc.R
+++ hTSC/step5_newCut_fold_rpkm1_etc.R
@@ -73,4 +73,234 @@ rownames(RPKM.LOG.SCALE)=rownames(COUNT)
 
 pca=prcomp(t(RPKM.LOG.SCALE),center = FALSE, scale. = FALSE)
 
+max_pc1=max(pca$x[,1])
+min_pc1=min(pca$x[,1])
+max_pc2=max(pca$x[,2])
+min_pc2=min(pca$x[,2])
+add_l=20
+
+plot(pca$x[,1],pca$x[,2],xlim=c(min_pc1-add_l,max_pc1+add_l),ylim=c(min_pc2-add_l,max_pc2+add_l),pch=16,
+     xlab='PC1',ylab='PC2')
+set.seed(123)
+text(label=rownames(pca$x),x=pca$x[,1], y=pca$x[,2], pos=sample(c(1,2,3,4),nrow(pca$x),replace = TRUE))
+
+
+
+
+
+
+####################################
+STAR_GENE=c('POU5F1','NANOG','SOX2','TFAP2C','KRT8','KRT18','LRP2','FLT1','KDR','CDX2','KRT7','VGLL1','GATA2','GATA3','TFAP2A','HAND1','MYC','ZIC2','NR2F2','ZIC5')
+STAR_GENE_INDEX=which(rownames(RPKM) %in% STAR_GENE)
+######################
+
+########################
+MAX_RPKM=apply(RPKM,1,max)
+MAX_RPKM[STAR_GENE_INDEX]
+##################
+IN_REP_DIFF= abs(RPKM.LOG.SCALE[,(1:8)*2]-RPKM.LOG.SCALE[,((1:8)*2-1)])
+MAX_IN_REP_DIFF=apply(IN_REP_DIFF,1,max)
+MAX_IN_REP_DIFF[STAR_GENE_INDEX]
+###################
+OUT_REP_RPKM=(RPKM[,(1:8)*2]  +  RPKM[,((1:8)*2-1)]   )/2
+
+LOGFD=c()
+i=1
+while(i<=ncol(OUT_REP_RPKM)){
+   j=i+1
+   while(j<=ncol(OUT_REP_RPKM)){
+      this_log_fd=log((OUT_REP_RPKM[,i]+0.01)/(OUT_REP_RPKM[,j]+0.01),2)
+      LOGFD=cbind(LOGFD, this_log_fd)
+      j=j+1
+      }
+   i=i+1
+   }
+
+ABSLOGFD=abs(LOGFD)
+
+MAX_ABSLOGFD=apply(ABSLOGFD,1,max)
+MAX_FD=2**MAX_ABSLOGFD
+MAX_FD[STAR_GENE_INDEX]
+##############################################
+
+
+used_gene_index=which(MAX_RPKM >1 & MAX_IN_REP_DIFF < 2 & MAX_FD > 2)
+
+
+length(which(STAR_GENE %in% names(used_gene_index)))
+
+
+
+
+library('ComplexHeatmap')
+library('circlize')
+library('seriation')
+
+mat=RPKM.LOG.SCALE[which(rownames(RPKM.LOG.SCALE) %in% names(used_gene_index)),]
+
+color_fun =colorRamp2(c(-1,-0.5,0,0.5,1 ), c('royalblue3','white','white','white','indianred3'))
+
+set.seed(12345)
+K=6
+C=kmeans(mat, K)$cluster
+NEW.C=C
+
+NEW.C[which(C==1)]=1
+NEW.C[which(C==2)]=2
+NEW.C[which(C==6)]=3
+NEW.C[which(C==3)]=4
+NEW.C[which(C==5)]=5
+NEW.C[which(C==4)]=6
+
+
+split <- paste0("C", NEW.C)
+
+o.mat=mat[order(NEW.C),]
+o.split=split[order(split)]
+
+Heatmap(o.mat,row_title='',name="Exp",
+        split=o.split, cluster_column_slices = FALSE, cluster_row_slices = FALSE,
+        cluster_columns=FALSE, cluster_rows=FALSE,
+	      show_column_dend = FALSE, show_row_dend = FALSE, 
+	      show_column_names=TRUE, show_row_names=FALSE,
+	      col=color_fun, border = TRUE,
+        left_annotation = rowAnnotation(foo = anno_block(gp = gpar(fill = 1:K)))
+        )
+
+OUT=cbind(o.split,o.mat)
+colnames(OUT)[1]='GENE\tCLUSTER'
+write.table(OUT,file='./RESULT/Heatmap_zvalue_withZcut.txt',sep='\t',row.names=T,col.names=T,quote=F)
+
+
+
+pdf('./RESULT/Heatmap_withZcut.pdf',width=5,height=4)
+
+HM <- Heatmap(o.mat,row_title='',name="Exp",
+        split=o.split, cluster_column_slices = FALSE, cluster_row_slices = FALSE,
+        cluster_columns=FALSE, cluster_rows=FALSE,
+	      show_column_dend = FALSE, show_row_dend = FALSE, 
+	      show_column_names=TRUE, show_row_names=FALSE,
+	      col=color_fun, border = TRUE,
+        left_annotation = rowAnnotation(foo = anno_block(gp = gpar(fill = 1:K)))
+        )
+       
+HM
+
+dev.off()   
+
+OUT=NEW.C[order(NEW.C)]
+write.table(OUT,file='./RESULT/GENE_CLUSTER_withZcut.txt',sep='\t',row.names=T,col.names=F,quote=F)
+
+
+
+
+###################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+used_gene_index=which(MAX_RPKM >1 & MAX_FD > 2)
+
+
+length(which(STAR_GENE %in% names(used_gene_index)))
+
+
+
+
+library('ComplexHeatmap')
+library('circlize')
+library('seriation')
+
+mat=RPKM.LOG.SCALE[which(rownames(RPKM.LOG.SCALE) %in% names(used_gene_index)),]
+
+color_fun =colorRamp2(c(-1,-0.5,0,0.5,1 ), c('royalblue3','white','white','white','indianred3'))
+
+set.seed(12345)
+K=6
+C=kmeans(mat, K)$cluster
+NEW.C=C
+
+NEW.C[which(C==2)]=1
+NEW.C[which(C==5)]=2
+NEW.C[which(C==1)]=3
+NEW.C[which(C==3)]=4
+NEW.C[which(C==6)]=5
+NEW.C[which(C==4)]=6
+
+
+split <- paste0("C", NEW.C)
+
+o.mat=mat[order(NEW.C),]
+o.split=split[order(split)]
+
+Heatmap(o.mat,row_title='',name="Exp",
+        split=o.split, cluster_column_slices = FALSE, cluster_row_slices = FALSE,
+        cluster_columns=FALSE, cluster_rows=FALSE,
+	      show_column_dend = FALSE, show_row_dend = FALSE, 
+	      show_column_names=TRUE, show_row_names=FALSE,
+	      col=color_fun, border = TRUE,
+        left_annotation = rowAnnotation(foo = anno_block(gp = gpar(fill = 1:K)))
+        )
+
+OUT=cbind(o.split,o.mat)
+colnames(OUT)[1]='GENE\tCLUSTER'
+write.table(OUT,file='./RESULT/Heatmap_zvalue_withoutZcut.txt',sep='\t',row.names=T,col.names=T,quote=F)
+
+
+
+pdf('./RESULT/Heatmap_withoutZcut.pdf',width=5,height=4)
+
+HM <- Heatmap(o.mat,row_title='',name="Exp",
+        split=o.split, cluster_column_slices = FALSE, cluster_row_slices = FALSE,
+        cluster_columns=FALSE, cluster_rows=FALSE,
+	      show_column_dend = FALSE, show_row_dend = FALSE, 
+	      show_column_names=TRUE, show_row_names=FALSE,
+	      col=color_fun, border = TRUE,
+        left_annotation = rowAnnotation(foo = anno_block(gp = gpar(fill = 1:K)))
+        )
+       
+HM
+
+dev.off()   
+
+OUT=NEW.C[order(NEW.C)]
+write.table(OUT,file='./RESULT/GENE_CLUSTER_withoutZcut.txt',sep='\t',row.names=T,col.names=F,quote=F)
+
+
+
+
+###################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
