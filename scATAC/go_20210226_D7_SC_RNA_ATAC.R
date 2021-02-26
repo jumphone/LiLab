ae053ed1392fb36188f24e0cfce7cdd65f7af659
Zhang, Feng <15110700005@fudan.edu.cn>
2021-02-26 09:54:14 +0800

    Update go_20210226_D7_SC_RNA_ATAC.R

--- scATAC/go_20210226_D7_SC_RNA_ATAC.R
+++ scATAC/go_20210226_D7_SC_RNA_ATAC.R
@@ -1,5 +1,33 @@
+BiocManager::install("EnsDb.Hsapiens.v86")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+
+
 
 
 /home/toolkit/tools/R4.0.3/bin/R
 
 setwd("/home/database/data/D7_SC_RNA_ATAC")
+
+library(reticulate)
+use_python("/home/toolkit/local/bin/python3",required=T)
+py_config()
+
+
+library(Signac)
+library(Seurat)
+library(GenomeInfoDb)
+library(EnsDb.Hsapiens.v86)
+library(ggplot2)
+library(patchwork)
+set.seed(1234)
+
+
+library(JASPAR2020)
+library(TFBSTools)
+library(BSgenome.Hsapiens.UCSC.hg38)
+
+
+
+
+
+
