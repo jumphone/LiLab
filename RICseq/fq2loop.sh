a656f2d995358731519589d0e8c7a08f9df0000a
Zhang, Feng <15110700005@fudan.edu.cn>
2023-10-24 15:22:22 +0800

    Update fq2loop.sh

--- RICseq/fq2loop.sh
+++ RICseq/fq2loop.sh
@@ -3,17 +3,15 @@ R2=/home/database/data/FIST2021/data/10k_SRR8179797_2.fastq
 OUT=/home/database/RICseq_demo/test2
 CPU=20
 RESOLUTION=1000
-
 ########
-# Human
+# Human hg19
 REF=/home/database/reference/hg19/hg19
 CHR_SIZE=/home/database/annotation/hg19/hg19_pure.chrom.sizes
+BLACK=/seq/ATAC-seq/Data/hg19_kundaje_blacklist.bed
 ########
-
 ##############################################################
 # Staring
 mkdir $OUT
-
 ##############################################################
 # QC & Mapping ...
 trim_galore --paired $R1 $R2 --length 20 -o $OUT --basename trim_galore_output
@@ -25,10 +23,13 @@ samtools sort $OUT\/read1.bam -o $OUT\/read1.sorted.bam
 samtools sort $OUT\/read2.bam -o $OUT\/read2.sorted.bam
 samtools rmdup -s $OUT\/read1.sorted.bam $OUT\/read1.sorted.rmdup.bam
 samtools rmdup -s $OUT\/read2.sorted.bam $OUT\/read2.sorted.rmdup.bam
-samtools merge $OUT\/merged.bam $OUT\/read1.sorted.rmdup.bam $OUT\/read2.sorted.rmdup.bam
+samtools index $OUT\/read1.sorted.rmdup.bam
+samtools index $OUT\/read2.sorted.rmdup.bam
+bedtools intersect -v -abam $OUT\/read1.sorted.rmdup.bam -b $BLACK > $OUT\/read1.sorted.rmdup.rmblack.bam
+bedtools intersect -v -abam $OUT\/read2.sorted.rmdup.bam -b $BLACK > $OUT\/read2.sorted.rmdup.rmblack.bam
+samtools merge $OUT\/merged.bam $OUT\/read1.sorted.rmdup.rmblack.bam $OUT\/read2.sorted.rmdup.rmblack.bam
 samtools sort -n $OUT\/merged.bam -o $OUT\/merged.paired.bam --threads $CPU
 bedtools bamtobed -bedpe -i $OUT\/merged.paired.bam >  $OUT\/merged.paired.bedpe
-
 ##############################################################
 # Formatting ...
 cooler cload pairs -c1 1 -p1 2 -c2 4 -p2 5 $CHR_SIZE\:$RESOLUTION   $OUT\/merged.paired.bedpe  $OUT\/mat_$RESOLUTION\.cool
@@ -36,9 +37,9 @@ hicConvertFormat --matrices  $OUT\/mat_$RESOLUTION\.cool  --outFileName  $OUT\/m
                  --inputFormat cool --outputFormat h5
 hicConvertFormat --matrices  $OUT\/mat_$RESOLUTION\.h5  --outFileName  $OUT\/mat_$RESOLUTION\.hicpro.mat \
                  --inputFormat h5 --outputFormat hicpro --bedFileHicpro $OUT\/mat_$RESOLUTION\.hicpro.bed
-
 ##############################################################
 # Loop calling ...
 mkdir $OUT\/mat_$RESOLUTION\.fithic
 /home/toolkit/local/bin/hicpro2fithic.py  -i $OUT\/mat_$RESOLUTION\.hicpro.mat -b $OUT\/mat_$RESOLUTION\.hicpro.bed -o $OUT\/mat_$RESOLUTION\.fithic -r $RESOLUTION
 /home/toolkit/local/bin/fithic -i $OUT\/mat_$RESOLUTION\.fithic/fithic.interactionCounts.gz -f $OUT\/mat_$RESOLUTION\.fithic/fithic.fragmentMappability.gz -o $OUT\/mat_$RESOLUTION\.fithic -r $RESOLUTION -x All
+
