63b20df4fafe0cf10c28c15afeb17ed7f30bb78b
Zhang, Feng <15110700005@fudan.edu.cn>
2023-10-24 10:23:56 +0800

    Update step01_formating.sh

--- RICseq/step01_formating.sh
+++ RICseq/step01_formating.sh
@@ -1,9 +1,16 @@
 
 
+
+# https://liz-fernandez.github.io/HiC-Langebio/04-matrix.html
+
 # https://github.com/4dn-dcic/pairix/blob/master/util/bam2pairs/README.md
 
-bam2pairs -l -c CHR_SIZE.txt    INPUT.bam    OUTPUT_PREFIX
+RESOLUTION=10000
+CHR_SIZE=
+INPUT_BAM=
+OUTPUT_PREFIX=
 
-cooler cload pairs -c1 2 -p1 3 -c2 4 -p2 5    CHR_SIZE.txt:10000     OUTPUT_PREFIX.bsorted.pairs     OUTPUT_PREFIX_10k.cool
+bam2pairs -l -c $CHR_SIZE    $INPUT_BAM   $OUTPUT_PREFIX
+cooler cload pairs -c1 2 -p1 3 -c2 4 -p2 5    $CHR_SIZE\:$RESOLUTION   $OUTPUT_PREFIX\.bsorted.pairs  $OUTPUT_PREFIX\_10k.cool
 
 
