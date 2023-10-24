f46c54c01a629bc28ed9968a2b08242565d5e50a
Zhang, Feng <15110700005@fudan.edu.cn>
2023-10-24 16:11:51 +0800

    Create step_clst.R

---
+++ Repli_seq/step_clst.R
@@ -0,0 +1,33 @@
+
+.stepClst<-function(MAT){
+    MAT=MAT
+    ######################
+    print('scaling...')
+    SMAT=t(apply(MAT,1,scale))
+    rownames(SMAT)=rownames(MAT)
+    colnames(SMAT)=colnames(MAT)
+    SMAT[which(is.na(SMAT))]=0
+    ###################
+    print('scoring...')
+    TMP=SMAT
+    TMP_ROW_RANK=t(apply(-TMP,1,rank,ties.method='random'))
+    TMP_FLAG=TMP_ROW_RANK
+    TMP_FLAG[which(TMP_ROW_RANK>1)]=0
+    ####################
+    COL_INDEX=c(1:ncol(TMP))
+    ####################
+    DDD=COL_INDEX - median(COL_INDEX)
+    WWW=  10 ** abs(DDD) * sign(DDD)
+    SCORE1 = .norm1(TMP %*% WWW )*0.99
+    SCORE2 = TMP_FLAG %*% COL_INDEX
+    SCORE = SCORE1 + SCORE2
+    SPLIT = as.integer(SCORE)
+    #####################
+    print('finished!')
+    #####################
+    OUT=list()
+    OUT$smat=SMAT
+    OUT$score=SCORE
+    OUT$split=SPLIT
+    return(OUT)
+    }
